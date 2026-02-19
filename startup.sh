c7ba64d046006d20cb8c12657e4861cbe573bffb
Dan Crawford <gdancc@gmail.com>
2026-02-19 17:59:18 +0000

    end handling for .env file path

--- startup.sh
+++ startup.sh
@@ -2,8 +2,31 @@
 
 # Expect to be run from the x-starter directory
 
+# Process CLI flags
+e_flag=".env"
+# Help log
+print_usage() {
+  printf "Usage: ...\n ./startup.sh -e [path to env file]\n"
+}
+
+while getopts 'e:' flag; do
+  case "${flag}" in
+    e) e_flag="${OPTARG}" ;;
+    *) print_usage
+       exit 1 ;;
+  esac
+done
+
+
+
+
 # Source env vars
-source .env
+if [ ! -f $e_flag ]; then
+  echo "Env file not found: $e_flag"
+  exit 1
+fi
+
+source $e_flag
 
 # Tickerplant
 # q tick.q [schema file] [log directory] -p [port number] < /dev/null > [log file] 2>&1 &
