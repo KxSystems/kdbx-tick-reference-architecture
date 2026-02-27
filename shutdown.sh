97e7a65254f32e52b0b6855dec0a6c24b54704c8
raiken2 <raiken@kx.com>
2026-02-27 13:28:30 +0000

    conflists on shutdown!

--- shutdown.sh
+++ shutdown.sh
@@ -2,7 +2,7 @@
 
 # Expect to be run from the x-starter directory
 
-# Get the process IDs of all q processes with a -procName argument for TP, RDB, or HDB and kill them
+# Get the process IDs of all q processes with a -procName argument for TP,FH, RDB, or HDB and kill them
 echo "Killed processes:"
 ps aux | grep "q.*-procName TP\|FH\|RDB\|HDB\|GW" | grep -v "grep" | while read line; do
     pid=$(echo "$line" | awk '{print $2}')
